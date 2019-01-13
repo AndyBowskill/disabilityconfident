c0d00f850f76f81fc7166e26f57dc52fa6fdb0f9
Andy Bowskill <andybowskill@gmail.com>
2019-01-13 20:46:06 +0000

    Some styling changes.

--- DisabledConfidentWebsite/wwwroot/js/site.js
+++ DisabledConfidentWebsite/wwwroot/js/site.js
@@ -1,4 +1,16 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+function loadAndDrawImage(url) {
+
+    var image = new Image();
+
+    image.onload = function () {
+        var c = document.getElementById("employers-that-have-signed-up");
+        var ctx = c.getContext("2d");
+        ctx.drawImage(image, 0, 0, 628, 741);
+    };
+
+    image.src = url;
+}
+loadAndDrawImage("images/employers_that_have_signed_up.png");
