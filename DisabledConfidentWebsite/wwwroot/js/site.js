1240b16c577383dcf8484e582d6a2e54cb90f4a5
Andy Bowskill <andybowskill@gmail.com>
2019-03-30 14:38:23 +0000

    Changes to the layout.

--- DisabledConfidentWebsite/wwwroot/js/site.js
+++ DisabledConfidentWebsite/wwwroot/js/site.js
@@ -1,7 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-function loadAndDrawImage(url) {
+﻿function loadAndDrawImage(url) {
 
     var image = new Image();
 
